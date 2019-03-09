1be3ef50dc8148c018d93e07b42ca2772b5e79cc
jtomaszewska <justyna.anna.tomaszewska@gmail.com>
2019-03-09 21:50:09 +0100

    iteracja po plikach i filtrowanie tekstowych

--- 01-file-channels/src/Futil.java
+++ 01-file-channels/src/Futil.java
@@ -1,7 +1,51 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
 /**
  * Created by Justynaa on 2019-03-09.
  */
 public class Futil {
-    public static void processDir(String dirName, String resultFileName) {
+    public static void processDir(String dirName, String resultFileName) throws IOException {
+
+
+        Files.walk(Paths.get(dirName))
+                .filter((path) -> Files.isRegularFile(path) && path.toString().endsWith("txt"))
+                .forEach(System.out::println);
+
+
+        File[] files = new File(dirName).listFiles();
+        //TODO sprawdzanie czy to plik tekstowy
+        for (File file :
+                files) {
+            if (file.isFile()){
+             //   saveFileText(dirName+"/"+file.getName());
+            }
+        }
+
+        //wczytywać te pliki (Cp1250) i dopisywać ich zawartości do pliku o nazwie TPO1res.txt utf8
+
+        //FileVisitor do przeglądania katalogu
+
+        //kanały plikowe (klasa FileChannel) do odczytu/zapisu plików
+        //        Path path = Paths.get(dirName);
+        //        FileChannel fileChannel = FileChannel.open(path);
+
+    }
+
+    private static void saveFileText(String filePath) throws IOException {
+        FileReader fileReader = new FileReader(filePath);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String textRead = bufferedReader.readLine();
+        System.out.println("File contents: ");
+
+        while (textRead != null) {
+          System.out.println("     " + textRead);
+            textRead = bufferedReader.readLine();
+        }
+        fileReader.close();
+        bufferedReader.close();
     }
 }
--- 01-file-channels/src/Main.java
+++ 01-file-channels/src/Main.java
@@ -1,11 +1,16 @@
+import java.io.IOException;
+
 /**
  * Created by Justynaa on 2019-03-09.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(System.getProperty("user.home"));
         String dirName = System.getProperty("user.home")+"/TPO1dir";
         String resultFileName = "TPO1res.txt";
         Futil.processDir(dirName, resultFileName);
+
+
+
     }
 }
