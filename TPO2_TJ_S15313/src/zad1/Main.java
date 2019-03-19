9026397868644b12a3ec3737d4bdcfac7d2ee312
jtomaszewska <justyna.anna.tomaszewska@gmail.com>
2019-03-19 23:17:19 +0100

    init

---
+++ TPO2_TJ_S15313/src/zad1/Main.java
@@ -0,0 +1,19 @@
+/**
+ *
+ *  @author Tomaszewska Justyna S15313
+ *
+ */
+
+package zad1;
+
+
+public class Main {
+  public static void main(String[] args) {
+    Service s = new Service("Poland");
+    String weatherJson = s.getWeather("Warsaw");
+    Double rate1 = s.getRateFor("USD");
+    Double rate2 = s.getNBPRate();
+    // ...
+    // część uruchamiająca GUI
+  }
+}
---
+++ TPO2_TJ_S15313/src/zad1/Service.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Tomaszewska Justyna S15313
+ *
+ */
+
+package zad1;
+
+
+public class Service {
+}  
