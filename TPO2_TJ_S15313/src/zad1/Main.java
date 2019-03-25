80a888fe7d7f199f1633928324f8594fca0d5c59
jtomaszewska <justyna.anna.tomaszewska@gmail.com>
2019-03-25 23:35:19 +0100

    GUI, weather and ECB rates

--- TPO2_TJ_S15313/src/zad1/Main.java
+++ TPO2_TJ_S15313/src/zad1/Main.java
@@ -7,13 +7,91 @@
 package zad1;
 
 
-public class Main {
-  public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
     Service s = new Service("Poland");
     String weatherJson = s.getWeather("Warsaw");
     Double rate1 = s.getRateFor("USD");
     Double rate2 = s.getNBPRate();
     // ...
     // część uruchamiająca GUI
-  }
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Informacje");
+
+        GridPane inputsGrid = new GridPane();
+        inputsGrid.setPadding(new Insets(10, 10, 10, 10));
+        inputsGrid.setVgap(8);
+        inputsGrid.setHgap(10);
+
+        Label cityLabel = new Label("Miasto:");
+        GridPane.setConstraints(cityLabel, 0, 0);
+
+        TextField cityInput = new TextField();
+        cityInput.setPromptText("Warszawa");
+        GridPane.setConstraints(cityInput, 2, 0);
+
+        Label countryLabel = new Label("Kraj:");
+        GridPane.setConstraints(countryLabel, 0, 1);
+
+        TextField countryInput = new TextField();
+        countryInput.setPromptText("Polska");
+        GridPane.setConstraints(countryInput, 2, 1);
+
+        Label weatherLabel = new Label("Informacja o aktualnej pogodzie:");
+        GridPane.setConstraints(weatherLabel, 0, 4);
+
+        TextArea weatherTextArea = new TextArea();
+        weatherTextArea.setPromptText(new Service("Poland").getWeather("Warszawa"));
+        weatherTextArea.setWrapText(true);
+        GridPane.setConstraints(weatherTextArea, 2, 4);
+
+        Label currencyOthersVal = new Label("Kurs waluty kraju do ");
+        GridPane.setConstraints(currencyOthersVal, 0, 5);
+
+        TextField currencyOthersNVal = new TextField();
+        GridPane.setConstraints(currencyOthersNVal, 2, 5);
+
+        ChoiceBox<String> availableCurrChoiceBox = new ChoiceBox<>();
+        availableCurrChoiceBox.getItems().addAll(new Service("").getCurrenciesFromECB());
+        availableCurrChoiceBox.setOnAction(event -> {
+            currencyOthersNVal.setText(new Service(countryInput.getText())
+                    .getRateFor(availableCurrChoiceBox.getValue()).toString());
+        });
+        GridPane.setConstraints(availableCurrChoiceBox, 1, 5);
+
+        Label currencyPLNLabel = new Label("Kurs PLN do waluty wybranego kraju:");
+        GridPane.setConstraints(currencyPLNLabel, 0, 6);
+
+        TextField currencyPLNVal = new TextField();
+        GridPane.setConstraints(currencyPLNVal, 2, 6);
+
+        Button checkButton = new Button("Sprawdź");
+        GridPane.setConstraints(checkButton, 2, 2);
+        checkButton.setOnAction(event -> {
+            weatherTextArea.setText(new Service(countryInput.getText()).getWeather(cityInput.getText()));
+        });
+
+        inputsGrid.getChildren().addAll(cityLabel, cityInput, countryLabel, countryInput, checkButton,
+                availableCurrChoiceBox,
+                weatherLabel, weatherTextArea,
+                currencyPLNLabel, currencyPLNVal,
+                currencyOthersVal, currencyOthersNVal);
+
+        Scene scene = new Scene(inputsGrid, 1000, 700);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
--- TPO2_TJ_S15313/src/zad1/Service.java
+++ TPO2_TJ_S15313/src/zad1/Service.java
@@ -1,11 +1,124 @@
 /**
- *
- *  @author Tomaszewska Justyna S15313
- *
+ * @author Tomaszewska Justyna S15313
  */
 
 package zad1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Service {
-}  
+
+    private static final String weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s";
+    private static final String weatherAppId = "76d057a60d6abd56ed36974936616945";
+    private static final String exchangeRateECBUrl = "https://api.exchangeratesapi.io/latest";
+    private String country;
+
+    public Service(String country) {
+        if (country != null)
+            this.country = country;
+    }
+
+    private static Map<String, String> getAvailableCurrencies() {
+        Locale[] locales = Locale.getAvailableLocales();
+        Map<String, String> currencies = new TreeMap<>();
+        for (Locale locale : locales) {
+            try {
+                currencies.put(locale.getDisplayCountry(),
+                        Currency.getInstance(locale).getCurrencyCode());
+            } catch (Exception e) {
+                // when the locale is not supported
+            }
+        }
+        return currencies;
+    }
+
+    /*informacja o pogodzie w formacie JSON*/
+    public String getWeather(String city) {
+        String httpResponse = "";
+        try {
+            URL url = new URL(String.format(weatherUrl, city, weatherAppId));
+            httpResponse = httpGet(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return httpResponse;
+    }
+
+    private String httpGet(URL url) throws IOException {
+//        System.out.println(url);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setDoOutput(true);
+
+        int status = con.getResponseCode();
+//        System.out.println(status);
+
+        if (status != 200) return "";
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+        return content.toString();
+    }
+
+    /*kurs waluty danego kraju wobec waluty podanej jako argument*/
+    public Double getRateFor(String currency_code) {
+        String basicCurr = getCurrency(country);
+        Map<String, Double> rates = null;
+        try {
+            rates = getRatesFor(basicCurr);
+        } catch (IOException e) {
+
+        }
+        return rates.get(currency_code);
+    }
+
+    private Map<String, Double> getRatesFor(String basicCurr) throws IOException {
+        String rateWithCountryCurrAsBasic = httpGet(new URL(exchangeRateECBUrl + "?base=" + basicCurr));
+        Map<String, Double> rates = new TreeMap<>();
+        Pattern pattern = Pattern.compile("\"([A-Z]{3})\":([0-9]*.[0-9]*)");
+        Matcher matcher = pattern.matcher(rateWithCountryCurrAsBasic);
+        while (matcher.find()) {
+            rates.put(matcher.group(1), Double.parseDouble(matcher.group(2)));
+        }
+        return rates;
+    }
+
+    private String getCurrency(String country) {
+        Map<String, String> currencies = getAvailableCurrencies();
+        String curr = "PLN";
+        if (currencies.keySet().contains(country)) {
+            curr = currencies.get(country);
+        }
+        return curr;
+    }
+
+    /*kurs złotego wobec waluty kraju*/
+    public Double getNBPRate() {
+        return null;
+    }
+
+    /*lista walut(kody) w european central bank api*/
+    public List<String> getCurrenciesFromECB() throws IOException {
+        String rates = httpGet(new URL(exchangeRateECBUrl));
+        List<String> currencies = new ArrayList<>();
+        Pattern pattern = Pattern.compile("[A-Z]{3}");
+        Matcher matcher = pattern.matcher(rates);
+        while (matcher.find()) {
+            currencies.add(matcher.group(0));
+        }
+        return currencies;
+    }
+}
